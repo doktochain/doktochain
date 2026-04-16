6b8becda38b5283adcdede4fc371028bb1ddd37b
Doktochain Inc <doktochain@gmail.com>
2026-04-16 10:30:16 -0400

    Updated vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -10,6 +10,7 @@ interface ImportMetaEnv {
   readonly VITE_S3_BUCKET: string;
   readonly VITE_CLOUDFRONT_DOMAIN: string;
   readonly VITE_AWS_REGION: string;
+  readonly VITE_SITE_URL?: string;
 }
 
 interface ImportMeta {
